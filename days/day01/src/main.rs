d4e3ce7630ab19544ca3cebda1efc67e9d58cb90
Leonardo Cerqueira <leonardoc.cerqueira96@gmail.com>
2021-12-01 19:18:00 -0300

    Add timers and implement part 2

--- days/day01/src/main.rs
+++ days/day01/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+use std::time::Instant;
 
 fn parse_input<T>(filename: T) -> io::Result<Vec<u32>>
 where
@@ -30,9 +31,7 @@ where
     Ok(depths)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let depths = parse_input("inputs/day01")?;
-
+fn part1(depths: &[u32]) -> u32 {
     let mut n_increases = 0;
     for i in 1..depths.len() {
         if depths[i] > depths[i - 1] {
@@ -40,6 +39,54 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("Depth increased {} times", n_increases);
+    n_increases
+}
+
+fn part2(depths: &[u32]) -> u32 {
+    let mut n_increases = 0;
+    let window_size = 3;
+
+    let mut window_iter = depths.windows(window_size);
+    let mut prev_window = window_iter.next().unwrap();
+    while let Some(window) = window_iter.next() {
+        let prev_sum: u32 = prev_window.iter().sum();
+        let next_sum: u32 = window.iter().sum();
+
+        if next_sum > prev_sum {
+            n_increases += 1;
+        }
+
+        prev_window = window;
+    }
+
+    n_increases
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Parse the input and time it
+    let t0 = Instant::now();
+    let depths = parse_input("inputs/day01")?;
+    let parse_time = t0.elapsed();
+
+    // Compute part 1 and time it
+    let t1 = Instant::now();
+    let n_increases_part1 = part1(&depths);
+    let part1_time = t1.elapsed();
+
+    // Compute part 2 and time it
+    let t2 = Instant::now();
+    let n_increases_part2 = part2(&depths);
+    let part2_time = t2.elapsed();
+
+    // Print results
+    let parse_time = parse_time.as_secs() as f64 + parse_time.subsec_nanos() as f64 * 1e-9;
+    println!("Parsing the input took {}s\n", parse_time);
+
+    let part1_time = part1_time.as_secs() as f64 + part1_time.subsec_nanos() as f64 * 1e-9;
+    println!("Part 1:\nTook {}s\nDepth increased {} times\n", part1_time, n_increases_part1);
+
+    let part2_time = part2_time.as_secs() as f64 + part2_time.subsec_nanos() as f64 * 1e-9;
+    println!("Part 2:\nTook {}s\nDepth increased {} times\n", part2_time, n_increases_part2);
+
     Ok(())
 }
