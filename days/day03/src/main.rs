a229818f9ccd93e69e7d7b71cb0ac040a50b87ac
Leonardo Cerqueira <leonardoc.cerqueira96@gmail.com>
2021-12-03 18:12:45 -0300

    Refactoring

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -5,75 +5,79 @@ use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::time::Instant;
 
-
 // Returns a vector, where each position holds the 1's count of that column, and the number of lines
-fn parse_input<T>(filename: T) -> io::Result<(Vec<usize>, usize)>
+fn parse_input<T>(filename: T) -> io::Result<Vec<String>>
 where
     T: AsRef<Path>,
 {
-    let mut ones_count = Vec::new();
-
     // Open input file
     let input = File::open(filename)?;
     let input_buf = BufReader::new(input);
 
-    // Read line by line
-    let mut line_count = 0;
-    for line_result in input_buf.lines() {
-        line_count += 1;
+    // Read and collect the lines
+    let lines: io::Result<Vec<String>> = input_buf.lines().collect();
 
-        let line = line_result?;
-
-        // Allocate the vector if its empty
-        if ones_count.is_empty() {
-            ones_count = vec![0; line.len()];
-        }
-
-        // Count the number of 1's
-        for (i, digit) in line.chars().enumerate() {
-            match digit {
-                '0' => continue,
-                '1' => ones_count[i] += 1,
-                c => println!("WARNING: Invalid character {}", c)
-            }
-        }
-    }
-
-    Ok((ones_count, line_count))
+    lines
 }
 
-fn part1(ones_count: &[usize], line_count: usize) -> (u64, u64) {
-    // Map vector of 1's counts into a binary string
-    let gamma_str: String = ones_count
+fn count_one_bits_by_column(binary_numbers: &[String]) -> Vec<usize> {
+    let n_columns = binary_numbers[0].len();
+
+    let n_one_bits = binary_numbers
         .iter()
-        .map(|c| {
-            if c * 2 < line_count {
-                '0'
-            } else {
-                '1'
+        .fold(vec![0; n_columns], |mut acc, number_str| {
+            for (i, digit) in number_str.chars().enumerate() {
+                if digit == '1' {
+                    acc[i] += 1;
+                }
             }
-        })
+
+            acc
+        });
+
+    n_one_bits
+}
+
+fn compute_gamma_rate(binary_numbers: &[String]) -> u32 {
+    // Get 1's count by column
+    let n_one_bits = count_one_bits_by_column(binary_numbers);
+
+    // Map vector of 1's counts into a binary string
+    let n_lines = binary_numbers.len();
+    let gamma_str: String = n_one_bits
+        .iter()
+        .map(|c| if c * 2 < n_lines { '0' } else { '1' })
         .collect();
 
-    // Convert binary string to u64
-    let gamma = u64::from_str_radix(&gamma_str, 2).unwrap();
-    
-    // Epsilon the inverse binary of gamma
-    // The mask is there to negate only 12 bits
-    let epsilon = (!gamma) & 0x0000000000000FFF;
+    // Convert binary string to u32
+    u32::from_str_radix(&gamma_str, 2).unwrap()
+}
 
-    (gamma, epsilon)
+fn compute_epsilon_rate(binary_numbers: &[String]) -> u32 {
+    // Get 1's count by column
+    let n_one_bits = count_one_bits_by_column(binary_numbers);
+
+    // Map vector of 1's counts into a binary string
+    let n_lines = binary_numbers.len();
+    let epsilon_str: String = n_one_bits
+        .iter()
+        .map(|c| if c * 2 < n_lines { '1' } else { '0' })
+        .collect();
+
+    // Convert binary string to u32
+    u32::from_str_radix(&epsilon_str, 2).unwrap()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Parse the input and time it
     let t0 = Instant::now();
-    let (ones_count, line_count) = parse_input("inputs/day03")?;
+    let binary_numbers = parse_input("inputs/day03")?;
     let parse_time = t0.elapsed();
 
     // Compute part 1 and time it
     let t1 = Instant::now();
-    let (gamma, epsilon) = part1(&ones_count, line_count);
+    let gamma = compute_gamma_rate(&binary_numbers);
+    let epsilon = compute_epsilon_rate(&binary_numbers);
     let part1_time = t1.elapsed();
 
     // Print results
@@ -82,7 +86,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let part1_time = part1_time.as_secs() as f64 + part1_time.subsec_nanos() as f64 * 1e-9;
     println!(
-        "Part 1:\nTook {}s\nGamma: {}\nEpsilon: {}\nMultiplied: {}\n",
+        "Part 1:\nTook {}s\nGamma rate: {}\nEpsilon rate: {}\nMultiplied: {}\n",
         part1_time,
         gamma,
         epsilon,
