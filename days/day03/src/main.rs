96dfe4b377940166a2f49970716ed8a48135b94a
Leonardo Cerqueira <leonardoc.cerqueira96@gmail.com>
2021-12-03 19:26:47 -0300

    Implement day 3, part 2

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -68,6 +68,70 @@ fn compute_epsilon_rate(binary_numbers: &[String]) -> u32 {
     u32::from_str_radix(&epsilon_str, 2).unwrap()
 }
 
+fn compute_oxygen_generator_rating(mut binary_numbers: Vec<String>) -> u32 {
+    // Get 1's count by column
+    let mut n_one_bits = count_one_bits_by_column(&binary_numbers);
+
+    let mut n_lines = binary_numbers.len();
+    let mut column_index = 0;
+    while binary_numbers.len() > 1 {
+        // Check which is the most common bit on column `column_index`
+        let most_common_bit = if n_one_bits[column_index] * 2 >= n_lines {
+            '1'
+        } else {
+            '0'
+        };
+
+        // Keep only numbers with `most_common_bit` on position `column_index`
+        binary_numbers = binary_numbers
+            .into_iter()
+            .filter(|n| n.chars().nth(column_index).unwrap() == most_common_bit)
+            .collect();
+
+        // Update number of lines
+        n_lines = binary_numbers.len();
+
+        // Update 1's count
+        n_one_bits = count_one_bits_by_column(&binary_numbers);
+
+        column_index += 1;
+    }
+
+    u32::from_str_radix(&binary_numbers[0], 2).unwrap()
+}
+
+fn compute_co2_scrubber_rating(mut binary_numbers: Vec<String>) -> u32 {
+    // Get 1's count by column
+    let mut n_one_bits = count_one_bits_by_column(&binary_numbers);
+
+    let mut n_lines = binary_numbers.len();
+    let mut column_index = 0;
+    while binary_numbers.len() > 1 {
+        // Check which is the least common bit on column `column_index`
+        let least_common_bit = if n_one_bits[column_index] * 2 >= n_lines {
+            '0'
+        } else {
+            '1'
+        };
+
+        // Keep only numbers with `most_common_bit` on position `column_index`
+        binary_numbers = binary_numbers
+            .into_iter()
+            .filter(|n| n.chars().nth(column_index).unwrap() == least_common_bit)
+            .collect();
+
+        // Update number of lines
+        n_lines = binary_numbers.len();
+
+        // Update 1's count
+        n_one_bits = count_one_bits_by_column(&binary_numbers);
+
+        column_index += 1;
+    }
+
+    u32::from_str_radix(&binary_numbers[0], 2).unwrap()
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Parse the input and time it
     let t0 = Instant::now();
@@ -80,6 +144,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let epsilon = compute_epsilon_rate(&binary_numbers);
     let part1_time = t1.elapsed();
 
+    // Compute part 2 and time it
+    let t2 = Instant::now();
+    let oxygen_generator_rating = compute_oxygen_generator_rating(binary_numbers.clone());
+    let co2_scrubber_rating = compute_co2_scrubber_rating(binary_numbers);
+    let part2_time = t2.elapsed();
+
     // Print results
     let parse_time = parse_time.as_secs() as f64 + parse_time.subsec_nanos() as f64 * 1e-9;
     println!("Parsing the input took {}s\n", parse_time);
@@ -91,6 +161,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         gamma,
         epsilon,
         gamma * epsilon
+    );
+
+    let part2_time = part2_time.as_secs() as f64 + part2_time.subsec_nanos() as f64 * 1e-9;
+    println!(
+        "Part 2:\nTook {}s\nOxygen Generator Rating: {}\nCO2 Scrubber Rating: {}\nLife Support Rating: {}\n",
+        part2_time,
+        oxygen_generator_rating,
+        co2_scrubber_rating,
+        oxygen_generator_rating * co2_scrubber_rating
     );
 
     Ok(())
