892f4b48b5a1b40961b69c6df81d0f660ff3f03a
Leonardo Cerqueira <leonardo.cerqueira@kryptus.com>
2021-12-27 09:01:47 -0300

    Fix day 23

--- days/day23/README.md
+++
@@ -1,1 +0,0 @@
-I did this one completely by hand, not the kind of problem I like to solve with code.
---
+++ days/day23/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!(
+        "I did this one completely by hand, not the kind of problem I like to solve with code."
+    );
+}
